eb6951760b3ead98b977c7b0338e4cc56adbdbc5
Kwangyoung <kwangyoung.kim@gmail.com>
2020-07-15 16:58:07 +0900

    Completed module3

--- src/index.js
+++ src/index.js
@@ -1,11 +1,56 @@
 // Create a tagged template lf`...` that formats text using LF line endings.
-var lf = () => { };
+var lf = (strings, ...values) => {
+  let resultString = '';
+
+  for (let i = 0; i < strings.length || i < values.length; i++) {
+    if (i < strings.length) {
+      resultString += transformLineEnding(strings[i], LineEndings.LF);
+    }
+    if (i < values.length && values[i]) {
+
+      resultString += values[i][disableConverter] ?
+        values[i].toString() :
+        transformLineEnding(values[i].toString(), LineEndings.LF);
+    }
+  }
+  return resultString;
+};
 
 // Create a tagged template cr`...` that formats text using CR line endings.
-var cr = () => { };
+var cr = (strings, ...values) => {
+  let resultString = '';
+
+  for (let i = 0; i < strings.length || i < values.length; i++) {
+    if (i < strings.length) {
+      resultString += transformLineEnding(strings[i], LineEndings.CR);
+    }
+    if (i < values.length && values[i]) {
+
+      resultString += values[i][disableConverter] ?
+        values[i].toString() :
+        transformLineEnding(values[i].toString(), LineEndings.CR);
+    }
+  }
+  return resultString;
+};
 
 // Create a tagged template crlf`...` that formats text using CRLF line endings.
-var crlf = () => { };
+var crlf = (strings, ...values) => {
+  let resultString = '';
+
+  for (let i = 0; i < strings.length || i < values.length; i++) {
+    if (i < strings.length) {
+      resultString += transformLineEnding(strings[i], LineEndings.CRLF);
+    }
+    if (i < values.length && values[i]) {
+
+      resultString += values[i][disableConverter] ?
+        values[i].toString() :
+        transformLineEnding(values[i].toString(), LineEndings.CRLF);
+    }
+  }
+  return resultString;
+};
 
 const transformLineEnding = (string, lineEnding) => {
   const { replaceCR, replaceLF, replaceCRLF } = LineEndingReplacements;
@@ -34,6 +79,8 @@ const LineEndings = {
   CRLF: CRLF
 };
 
+const disableConverter = Symbol.for("crlf-converter-disable");
+
 const LineEndingReplacements = {
   replaceCR: (string, newEnding) =>
     string.replace(/(\r+)([^\n]|$)/g, (_match, p1, p2) => {
@@ -53,5 +100,6 @@ module.exports = {
   cr,
   crlf,
   LineEndings,
-  transformLineEnding
+  transformLineEnding,
+  disableConverter
 };
--- src/index.spec.js
+++ src/index.spec.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 const { describe, it } = require("mocha");
+const { expect, assert } = require("chai");
 const { lf } = global.lf || require("index.js");
 
 describe("CRLF Converter", () => {
@@ -8,18 +9,21 @@ describe("CRLF Converter", () => {
       const result = lf`\r\nHello there.`;
 
       // Use `String.prototype.startsWith()`
+      expect(result.startsWith(`\nH`)).to.be.true;
     });
 
     it("should replace CRLF at the end of a string", () => {
       const result = lf`Hello there.\r\n`;
 
       // Use `String.prototype.endsWith()`
+      expect(result.endsWith(`.\n`)).to.be.true;
     });
 
     it("should replace CRLF in the middle of a string", () => {
       const result = lf`Hello \r\n\r\nthere.`;
 
       // Use `String.prototype.includes()`
+      expect(result.includes(`\n\n`)).to.be.true;
     });
   });
 });
